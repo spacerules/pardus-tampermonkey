8e2cd23a41b5d683ba5885fb4f28a2f93b7adc29
Samuel Haffner <haffnersam@gmail.com>
2025-07-28 14:15:06 -0500

    Update LogTotalList.user.js
    adding in correct requires for the cookies.js file

--- userjs/LogTotalList.user.js
+++ userjs/LogTotalList.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Pardus Log Total List
 // @namespace    https://github.com/spacerules/pardus-tampermonkey
-// @version      1.0.2
+// @version      1.0.3
 // @description  adds a total to the log files
 // @author       Spacerules
 // @match        http://*.pardus.at/*
 // @match        https://*.pardus.at/*
 // @include      http*://*.pardus.at/overview*
 // @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/Logger.js
+// @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/cookies.js
 // @icon         https://avatars.githubusercontent.com/u/2374313?v=4
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/userjs/LogTotalList.user.js
@@ -15,6 +16,7 @@
 // ==/UserScript==
 
 /* global logSuccess, logError, logInfo, logWarn, logDebug, logGroupStart, logGroupEnd */
+/* global readCookie, writeCookie */
 
 (function() {
     'use strict';
