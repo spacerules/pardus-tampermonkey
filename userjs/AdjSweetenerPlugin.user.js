c7e0bd382c9c6aab29d525362fcbe6c1445ff062
Spacerules <chrismontgomery77@gmail.com>
2025-07-29 02:38:11 -0500

    Update AdjSweetenerPlugin.user.js
    fixed the username to stay in the screen when you move and the screen refreshs

--- userjs/AdjSweetenerPlugin.user.js
+++ userjs/AdjSweetenerPlugin.user.js
@@ -1,74 +1,110 @@
 // ==UserScript==
-// @name         Pardus Adjust Sweetener pluging
+// @name         Pardus Adjust Sweetener Plugin
 // @namespace    https://github.com/spacerules/pardus-tampermonkey
-// @version      1.0.1
-// @description  changes the location of certain elements so the map is not overlapped
+// @version      1.0.4
+// @description  Moves interface elements to avoid overlap with the map and injects username
 // @author       Spacerules
 // @match        http*://*.pardus.at/msgframe.php
-// @match        https://*.pardus.at/msgframe.php
 // @match        http*://*.pardus.at/main.php
-// @match        https://*.pardus.at/main.php
 // @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/Logger.js
 // @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/cookies.js
 // @icon         https://avatars.githubusercontent.com/u/2374313?v=4
-// @grant        none
+// @grant        unsafeWindow
 // @updateURL    https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/userjs/AdjSweetenerPlugin.user.js
 // @downloadURL  https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/userjs/AdjSweetenerPlugin.user.js
 // ==/UserScript==
 
 /* global logSuccess, logError, logInfo, logWarn, logDebug, logGroupStart, logGroupEnd */
-/* global readCookie, writeCookie */
-(function() {
+/* global readCookie */
+(function () {
     'use strict';
 
-    const LOGGING_ENABLED = false;
-
+    const LOGGING_ENABLED = true;
     logGroupStart(LOGGING_ENABLED, 'File: AdjSweetenerPlugin.user.js');
 
     function addusername() {
-        logGroupStart(LOGGING_ENABLED, 'Function: adjSweetener');
+        logGroupStart(LOGGING_ENABLED, 'Function: addusername');
         let username = readCookie('user');
-        logDebug(LOGGING_ENABLED, 'Username: ', username);
+        logDebug(LOGGING_ENABLED, 'Username:', username);
 
-        var statuselem = document.getElementById('status_content');
-        var tbody = statuselem.querySelector('table tbody');
-        var newRow = document.createElement('tr');
+        let statuselem = document.getElementById('status_content');
+        if (!statuselem) {
+            logWarn(LOGGING_ENABLED, 'status_content not found');
+            logGroupEnd(LOGGING_ENABLED);
+            return;
+        }
+
+        let tbody = statuselem.querySelector('table tbody');
+        if (!tbody) {
+            logWarn(LOGGING_ENABLED, 'No tbody found under #status_content');
+            logGroupEnd(LOGGING_ENABLED);
+            return;
+        }
+
+        if (document.getElementById('tdStatusUserName')) {
+            logDebug(LOGGING_ENABLED, 'Username row already exists.');
+            logGroupEnd(LOGGING_ENABLED);
+            return;
+        }
+
+        let newRow = document.createElement('tr');
         newRow.vAlign = "middle";
 
-        var newCell = document.createElement('td');
-        newCell.innerHTML = '<b><u>'+username+'</u></b>';
-        newCell.align="center";
-        newCell.id="tdStatusUserName";
-        newCell.colSpan=4;
+        let newCell = document.createElement('td');
+        newCell.innerHTML = `<b><u>${username}</u></b>`;
+        newCell.align = "center";
+        newCell.id = "tdStatusUserName";
+        newCell.colSpan = 4;
+
         newRow.appendChild(newCell);
-
-
         tbody.insertBefore(newRow, tbody.firstChild);
         logGroupEnd(LOGGING_ENABLED);
     }
 
-    // tightwad update
+    function patchUpdateStatus() {
+        let uw;
+        try {
+            uw = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window;
+        } catch (e) {
+            uw = window;
+        }
+
+        if (uw.updateStatus && !uw.updateStatus._patchedBySweetener) {
+            const originalUpdateStatus = uw.updateStatus;
+
+            uw.updateStatus = function (a) {
+                originalUpdateStatus.call(this, a);
+                logDebug(LOGGING_ENABLED, 'updateStatus patched, reinserting username');
+                try {
+                    addusername();
+                } catch (e) {
+                    logError(LOGGING_ENABLED, 'addusername() failed in updateStatus:', e);
+                }
+            };
+
+            uw.updateStatus._patchedBySweetener = true;
+        } else if (!uw.updateStatus) {
+            logWarn(LOGGING_ENABLED, 'updateStatus not yet defined; retrying...');
+            setTimeout(patchUpdateStatus, 250);
+        }
+    }
+
     function updtightwad() {
         logGroupStart(LOGGING_ENABLED, 'Function: updtightwad');
         waitForElement('#tightwad_overview', (elem) => {
             elem.style.position = "";
-            var brtag1 = document.createElement('br');
-            var brtag2 = document.createElement('br');
-            var brtag3 = document.createElement('br');
-            var brtag4 = document.createElement('br');
-
+            const br = () => document.createElement('br');
             const tdTabsRight = document.getElementById('tdTabsRight');
 
-            tdTabsRight.insertBefore(brtag1, document.getElementById('otherships'));
-            tdTabsRight.insertBefore(brtag2, document.getElementById('otherships'));
+            tdTabsRight.insertBefore(br(), document.getElementById('otherships'));
+            tdTabsRight.insertBefore(br(), document.getElementById('otherships'));
             tdTabsRight.style.width = "272px";
-            tdTabsRight.appendChild(brtag3);
-            tdTabsRight.appendChild(brtag4);
+            tdTabsRight.appendChild(br());
+            tdTabsRight.appendChild(br());
         });
         logGroupEnd(LOGGING_ENABLED);
     }
 
-    // clock update
     function waitForElement(selector, callback, timeout = 5000) {
         const interval = 50;
         const start = Date.now();
@@ -76,53 +112,45 @@
         const check = () => {
             const el = document.querySelector(selector);
             if (el) return callback(el);
-            if (Date.now() - start > timeout) return; // Give up after timeout
-            setTimeout(check, interval);
+            if (Date.now() - start < timeout) setTimeout(check, interval);
         };
-
         check();
     }
 
-    // Usage:
     function adjSweetener() {
-
         logGroupStart(LOGGING_ENABLED, 'Function: adjSweetener');
-
         waitForElement('body > div', (div) => {
-
             div.style.position = "";
             div.style.margin = "0px 0px 5px 0px";
-            //Main Table > Table Body > TR0 top/only row, top row is pictures, td2 right column aligned middle,
-            var thirdTd = document.getElementsByTagName("Table")[0].children[0].children[0].children[2];
+
+            const table = document.getElementsByTagName("Table")[0];
+            const thirdTd = table?.rows[0]?.cells[2];
             if (thirdTd) {
-                thirdTd.insertBefore(div, thirdTd.firstChild)
+                thirdTd.insertBefore(div, thirdTd.firstChild);
             }
         });
         logGroupEnd(LOGGING_ENABLED);
-
     }
 
-function pardusAdjSweetenerInit() {
-    // Load Document data into short variables
-    var doc = document;
-    var loc = doc.location.href;
+    function pardusAdjSweetenerInit() {
+        const loc = document.location.href;
+        logGroupStart(LOGGING_ENABLED, `Init | loc=${loc}`);
 
-    logGroupStart(LOGGING_ENABLED, 'Function: pardusAdjSweetenerInit | loc=' + loc);
-    if (loc.match('msgframe.php')) {
-        logSuccess(LOGGING_ENABLED, 'location matched:', loc);
-        adjSweetener();
-    }
-    else if (loc.match('main.php')) {
-       logSuccess(LOGGING_ENABLED, 'location matched:', loc);
-       addusername();
-       updtightwad();
+        if (loc.includes('msgframe.php')) {
+            logSuccess(LOGGING_ENABLED, 'Location matched msgframe.php');
+            adjSweetener();
+        } else if (loc.includes('main.php')) {
+            logSuccess(LOGGING_ENABLED, 'Location matched main.php');
+
+            // No longer call addusername or observeStatusTable directly
+            patchUpdateStatus();
+            addusername();
+            updtightwad();
+        }
+
+        logGroupEnd(LOGGING_ENABLED);
     }
 
+    pardusAdjSweetenerInit();
     logGroupEnd(LOGGING_ENABLED);
-}
-
-
-  pardusAdjSweetenerInit();
-
-  logGroupEnd(LOGGING_ENABLED);
 })();
