ea05e4c7ea268e0580a4a19dea38419dd1e39c46
Spacerules <chrismontgomery77@gmail.com>
2025-08-12 12:54:19 -0500

    Update dockScreen.user.js

--- userjs/dockScreen.user.js
+++ userjs/dockScreen.user.js
@@ -1,85 +1,77 @@
 // ==UserScript==
-// @name         Dock screen updater
+// @name         Quick Links
 // @namespace    https://github.com/spacerules/pardus-tampermonkey
-// @version      1.0.3
-// @description  gives the calculated production upkeep for the buildings
+// @version      1.0.1
+// @description  Quick Links
 // @author       Spacerules
-// @match        http://*.pardus.at/logout.php
-// @match        https://*.pardus.at/logout.php
+// @match        http://*.pardus.at/main.php
+// @match        https://*.pardus.at/main.php
 // @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/Logger.js
 // @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/cookies.js
 // @icon         https://avatars.githubusercontent.com/u/2374313?v=4
-// @updateURL    https://github.com/spacerules/pardus-tampermonkey/raw/refs/heads/main/userjs/dockScreen.user.js
-// @downloadURL  https://github.com/spacerules/pardus-tampermonkey/raw/refs/heads/main/userjs/dockScreen.user.js
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/userjs/quickLinks.user.js
+// @downloadURL  https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/userjs/quickLinks.user.js
 // ==/UserScript==
 
 /* global logSuccess, logError, logInfo, logWarn, logDebug, logGroupStart, logGroupEnd, logEnabled, logTable */
 /* global readCookie, writeCookie */
-
 (function() {
     'use strict';
 
   logEnabled(false);
 
-  logGroupStart('File: dockScreen.js');
+var orionCustomLinks = [
+  "https://pardusmapper.com/" + readCookie("uni") + "/" + readCookie("sector"),
+  'https://pardus.maxisoft.org/monsters.html',
+  'http://pardus.maxisoft.org/monsterskillfinder.html',
+  'https://docs.google.com/spreadsheets/d/1UaITDxjb2eQEtApp4N5h3L4bDd7Z5BJsl4H6bI8cmJU/edit?pli=1&gid=0#gid=0',
+  'https://thewaistelands.info/pardus-clock/'
+];
 
-  function AddSectorLink() {
-    logGroupStart('Function: AddSectorLink');
-    var tdStatusSector = document.getElementById("tdStatusSector");
+var orionCustomLinkNames = [
+  'Map',
+  'Monsters',
+  'Monster Skill Finder',
+  'Enemy&nbsp;Bible',
+  "Clocks"
+];
 
-    if (tdStatusSector) {
-        // Get the current text inside the td (e.g., " UG 5-093")
-        var text = tdStatusSector.textContent.trim();
-        logDebug(text);
-        // Create the link
-        var link = document.createElement("a");
-        link.href = "https://pardusmapper.com/" + readCookie("uni") + "/" + encodeURIComponent(text);
-        link.target = "_blank"; // Open in new window/tab
-        link.rel = "noopener noreferrer"; // Security best practice
-        link.innerHTML = text;
+// Locate the original table
+var originalTable = document.getElementById('yourship');
+var clonedTable = originalTable.cloneNode(true);
+clonedTable.id = 'yourship_custom_links';
 
-        logDebug(link.href);
-        // Replace the contents of the td with the link
-        tdStatusSector.innerHTML = "&nbsp;";
-        tdStatusSector.appendChild(link);
-    }
+// Replace content inside the internal content <div>
+var contentDiv = clonedTable.querySelector('#yourship_content');
 
-    logGroupEnd();
-  }
+var customLinksHTML = '<table border="0" align="center"><tbody>';
+for (let i = 0; i < orionCustomLinks.length; i++) {
+  customLinksHTML += `
+    <tr>
+      <td colspan="2" style="text-align:center">
+        <a href="${orionCustomLinks[i]}" target="_blank">
+          <font size="1">${orionCustomLinkNames[i]}</font>
+        </a>
+      </td>
+    </tr>
+  `;
+}
+customLinksHTML += '</tbody></table>';
+contentDiv.innerHTML = customLinksHTML;
 
-  function AddUsername() {
-    logGroupStart('Function: AddUsername');
+// Replace the ship image
+var imageElem = clonedTable.querySelector('#yourship_image');
+if (imageElem) {
+  imageElem.src = 'https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/resources/links.png';
+}
 
-    //Main Table > Table Body > TR1 middle row, top row is pictures, td1 middle column aligned middle, First item is the header we are appending to
-    var h1data = document.getElementsByTagName("Table")[0].children[0].children[1].children[1].children[0];
-      var text = h1data.textContent.trim() +"  " +readCookie("user");
-    h1data.innerHTML = text;
-    logDebug(text);
+// Insert two <br> elements and the modified table after the original
+var br1 = document.createElement('br');
+var br2 = document.createElement('br');
+originalTable.parentNode.insertBefore(br1, originalTable.nextSibling);
+originalTable.parentNode.insertBefore(br2, br1.nextSibling);
+originalTable.parentNode.insertBefore(clonedTable, br2.nextSibling);
 
-    logGroupEnd();
 
-  }
-  // Your code here..
-  function pardusBuildingInit() {
-    // Load Document data into short variables
-    var doc = document;
-    var loc = doc.location.href;
-
-    logGroupStart('Function: pardusBuildingInit | loc=' + loc);
-    //  not used in this script but want to keep it for potential mods.
-    //  var search = doc.location.search.substring(doc.location.search.indexOf("=") + 1);
-
-    if (loc.match('logout.php')) {
-      logSuccess('location matched:', loc);
-        AddUsername();
-        AddSectorLink();
-    }
-
-    logGroupEnd();
-
-  }
-
-  pardusBuildingInit();
-
-  logGroupEnd();
 })();
