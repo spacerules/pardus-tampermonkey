1d4a50e4902240db8e707f844429c0c7a8db7e48
Spacerules <chrismontgomery77@gmail.com>
2025-08-12 13:01:06 -0500

    Update MonsterSkillFinder.user.js

--- userjs/MonsterSkillFinder.user.js
+++ userjs/MonsterSkillFinder.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Monster Skill Finder
 // @namespace    https://github.com/spacerules/pardus-tampermonkey
-// @version      1.0.1
+// @version      1.0.2
 // @description  try to take over the world!
 // @author       Spacerules
 // @match        http://*.pardus.at/overview_stats.php
