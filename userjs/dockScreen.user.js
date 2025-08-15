97a950796e0a3ac1a212b121cdbd6651a89481cb
Spacerules <chrismontgomery77@gmail.com>
2025-08-15 01:16:06 -0500

    Update dockScreen.user.js

--- userjs/dockScreen.user.js
+++ userjs/dockScreen.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Dock screen updater
 // @namespace    https://github.com/spacerules/pardus-tampermonkey
-// @version      1.0.3
+// @version      1.0.4
 // @description  gives the calculated production upkeep for the buildings
 // @author       Spacerules
 // @match        http://*.pardus.at/logout.php
 // @match        https://*.pardus.at/logout.php
-// @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/Logger.js
-// @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/cookies.js
+// @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/Logger.user.js
+// @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/cookies.user.js
 // @icon         https://avatars.githubusercontent.com/u/2374313?v=4
 // @updateURL    https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/userjs/dockScreen.user.js
 // @downloadURL  https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/userjs/dockScreen.user.js
