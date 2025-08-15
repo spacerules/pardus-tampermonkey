4d3dbfee6b1421c7700f6069baee7bee265e2636
Spacerules <chrismontgomery77@gmail.com>
2025-08-15 01:16:54 -0500

    Update ship2opponent.user.js

--- userjs/ship2opponent.user.js
+++ userjs/ship2opponent.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         ship2opponent
 // @namespace    https://github.com/spacerules/pardus-tampermonkey
-// @version      1.0.2
+// @version      1.0.3
 // @description  try to take over the world!
 // @author       Spacerules
 // @match        http://*.pardus.at/ship2opponent_combat.php
 // @match        https://*.pardus.at/ship2opponent_combat.php
 // @include      http*://*.pardus.at/ship2opponent_combat.php*
-// @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/Logger.js
-// @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/cookies.js
+// @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/Logger.user.js
+// @require      https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/global-files/cookies.user.js
 // @icon         https://avatars.githubusercontent.com/u/2374313?v=4
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/spacerules/pardus-tampermonkey/main/userjs/ship2opponent.user.js
