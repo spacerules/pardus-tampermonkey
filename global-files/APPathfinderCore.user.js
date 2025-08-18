38ce79ed9528d40614417845b6fc662a67f6cee7
Spacerules <chrismontgomery77@gmail.com>
2025-08-18 13:42:30 -0500

    Update APPathfinderCore.user.js
    Xholes cant map back to themselves

--- global-files/APPathfinderCore.user.js
+++ global-files/APPathfinderCore.user.js
@@ -167,6 +167,7 @@
 
             if(beacon && beacon.type==="xh"){
                 for(const targetSector of XHOLE_SECTORS){
+                    if (targetSector === sector) continue;
                     const targetMap = await loadSector(targetSector);
                     for(const target of targetMap.beaconList.filter(b=>b.type==="xh")){
                         if(targetSector===sector && target.x===x && target.y===y) continue;
